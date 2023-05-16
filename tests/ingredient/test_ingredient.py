fced7ef8b01d5c74ebe24143d4693e9751b1218d
Rafael Souza <souza_rafael.97@outlook.com>
2023-05-16 20:32:59 -0300

    refactor: req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,32 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Iniciando o Projeto
-# # Req 1
+# Req 1
 def test_ingredient():
-    pass
+    mozzarella_ingredient = Ingredient("queijo mussarela")
+    assert mozzarella_ingredient.name == "queijo mussarela"
+    assert mozzarella_ingredient.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+        }
+    assert mozzarella_ingredient.__eq__(Ingredient("queijo mussarela"))
+    assert mozzarella_ingredient.__eq__(Ingredient("ovo")) is False
+    assert hash(mozzarella_ingredient) == hash("queijo mussarela")
+    assert hash(mozzarella_ingredient) != hash(Ingredient("ovo"))
+    assert mozzarella_ingredient.__repr__() == "Ingredient('queijo mussarela')"
+
+    flour_ingredient = Ingredient("farinha")
+    assert flour_ingredient.name == "farinha"
+    assert flour_ingredient.restrictions == {Restriction.GLUTEN}
+    assert flour_ingredient.__eq__(Ingredient("farinha"))
+    assert flour_ingredient.__eq__(Ingredient("ovo")) is False
+    assert hash(flour_ingredient) == hash("farinha")
+    assert hash(flour_ingredient) != hash(Ingredient("ovo"))
+    assert flour_ingredient.__repr__() == "Ingredient('farinha')"
+
+    salt_ingredient = Ingredient("sal")
+    assert salt_ingredient.name == "sal"
+    assert salt_ingredient.__eq__(Ingredient("sal"))
+    assert salt_ingredient.__eq__(Ingredient("ovo")) is False
+    assert hash(salt_ingredient) == hash("sal")
+    assert hash(salt_ingredient) != hash(Ingredient("ovo"))
+    assert salt_ingredient.__repr__() == "Ingredient('sal')"
